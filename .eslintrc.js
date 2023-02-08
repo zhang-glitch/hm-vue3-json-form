acac347fac83f48f3ccc8aebcadaec0e74c3df05
zhanghaozhang <zhanghaozhang@dianhun.cn>
2023-02-08 13:55:42 +0800

    [docs] 项目初始化

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    'plugin:prettier/recommended',
+  ],
+  // parser: 'vue-eslint-parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    // sourceType: 'module',
+    // parser: '@typescript-eslint/parser',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        parser: 'flow',
+        semi: false,
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+}
